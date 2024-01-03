f8e3b26fc39fd73c57dc0098c7a51de2300d7d02
ZygimantasB <92627436+ZygimantasB@users.noreply.github.com>
2024-01-03 16:29:21 +0200

    Dict solutions

--- pynative/dictionary_exercise.py
+++ pynative/dictionary_exercise.py
@@ -11,7 +11,7 @@ def create_dict(keys1, values1):
     # return combined_lists
 
 
-print(create_dict(keys, values))
+# print(create_dict(keys, values))
 
 
 # Exercise 2: Merge two Python dictionaries into one
@@ -26,7 +26,7 @@ def merge_two_dictionaries(sample_dict1, sample_dict2):
     return combined_dict
 
 
-print(merge_two_dictionaries(dict1, dict2))
+# print(merge_two_dictionaries(dict1, dict2))
 
 # Exercise 3: Print the value of key ‘history’ from the below dict
 
@@ -42,7 +42,7 @@ sampleDict = {
     }
 }
 
-print(sampleDict['class']['student']['marks']['history'])
+# print(sampleDict['class']['student']['marks']['history'])
 
 
 employees = ['Kelly', 'Emma']
@@ -58,5 +58,54 @@ def initialize_dictionary_values(sample_list, sample_dict):
     return dict.fromkeys(sample_list, sample_dict)
 
 
-print(initialize_dictionary_values(employees, defaults))
+# print(initialize_dictionary_values(employees, defaults))
 
+# Exercise 5: Create a dictionary by extracting the keys from a given dictionary
+
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"}
+
+# Keys to extract
+keys = ["name", "salary"]
+
+
+def create_dictionary_from_keys(my_dict, extract_keys):
+    return {key: my_dict[key] for key in extract_keys}
+
+
+# print(create_dictionary_from_keys(sample_dict, keys))
+
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"
+}
+
+# Keys to remove
+keys = ["name", "salary"]
+
+# Exercise 6: Delete a list of keys from a dictionary
+
+
+def remove_keys(sample_dict1, remove_keys1):
+    return {key: sample_dict1[key] for key in sample_dict1 if key not in remove_keys1}
+
+
+print(remove_keys(sample_dict, keys))
+
+# Exercise 7: Check if a value exists in a dictionary
+
+sample_dict7 = {'a': 100, 'b': 200, 'c': 300}
+
+
+def check_insisting_value(my_sample_dict, checking_value):
+    # return checking_value in my_sample_dict.values()
+    # return any(value == checking_value for value in my_sample_dict.values())
+    return checking_value in [value for value in my_sample_dict.values()]
+
+print(check_insisting_value(sample_dict7, '200'))
+print(check_insisting_value(sample_dict7, 200))

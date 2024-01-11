eaccf07dec7727d2bde7d9e86cc6a751f215fe5a
ZygimantasB <92627436+ZygimantasB@users.noreply.github.com>
2024-01-11 14:23:18 +0200

    Code wars solution

--- code_wars/kata_7.py
+++ code_wars/kata_7.py
@@ -7,5 +7,27 @@ def nb_year(p0, percent, aug, p):
    return years
 
 
-print(nb_year(1500, 2, 000, 5000), 15)
-print((nb_year(1500000, 2.5, 10000, 2000000), 10))
+# print(nb_year(1500, 2, 000, 5000), 15)
+# print((nb_year(1500000, 2.5, 10000, 2000000), 10))
+
+
+# def num_of_open_lockers(lockers):
+#     count = 1000
+#     for locker in range(1, lockers + 1):
+#         if locker % 2 == 0:
+#             count -= 1
+#         if locker % 3 == 0:
+#             count += 1
+#         if locker % 4 == 0:
+#             count += 1
+#     return count
+
+def num_of_open_lockers(n):
+    square_root = 0
+    while (square_root + 1) ** 2 <= n:
+        square_root += 1
+    return square_root
+
+
+
+print(num_of_open_lockers(1000))
--- code_wars/kata_8.py
+++ code_wars/kata_8.py
@@ -10,3 +10,18 @@
 
 # def pluck(objs, name):
 #     return [obj.get(name, None) for obj in objs]
+
+def digitize(numbers):
+    return [int(number)for number in str(numbers)[::-1]]
+
+
+# print(digitize(35231))
+
+def square(number):
+    return number * number
+
+
+numbers = [1, 2, 3, 4, 5]
+
+result = list(map(square, numbers))
+print(result)

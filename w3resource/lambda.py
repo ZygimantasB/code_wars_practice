8eb144328278d47844dc825090c41c8aea60c089
zygimantas <zygimantuxd@gmail.com>
2024-02-05 08:57:55 +0200

    find_intersection

--- w3resource/lambda.py
+++ w3resource/lambda.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+from functools import reduce
+
+
 class LambdaSolutions:
 
     def __init__(self):
@@ -5,6 +9,12 @@ class LambdaSolutions:
         self.dictionary_values = [{'make': 'Nokia', 'model': 216, 'color': 'Black'}, {'make': 'Samsung', 'model': 7,
                                                                                   'color': 'Blue'}, {'make': 'Mi Max', 'model': '2', 'color': 'Gold'}]
         self.numbers_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        self.weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+        self.sentence = 'My name is Friday'
+        self.date_time = datetime(2020, 1, 15, 9, 3, 32)
+        self.string_number_list = ['a', 1, 'hello', 2, 'world', 3, 'python', 4, 'is', 5, 'awesome']
+        self.numbers1 = [1, 2, 3, 5, 7, 8, 9, 10]
+        self.numbers2 = [1, 2, 4, 8, 9]
 
 
     def add_numbers(self, number):
@@ -31,10 +41,60 @@ class LambdaSolutions:
         even_numbers = list(filter(lambda number: number % 2 == 0, self.numbers_list))
         return odd_numbers, even_numbers
 
+    def check_length(self):
+        return list(filter(lambda day: len(day) == 6, self.weekdays))
+
+    def make_square_list(self):
+        return list(map(lambda number: number ** 2, self.numbers_list))
+
+    def make_cube_list(self):
+        return list(map(lambda number: number ** 3, self.numbers_list))
+
+    def check_starting_work(self):
+        return any(list(map(lambda word: word.startswith('My'), self.sentence.split())))
+
+    def extract_date_time(self):
+        # return {
+        #     'year': self.date_time.year,
+        #     'month': self.date_time.month,
+        #     'day': self.date_time.day,
+        #     'time': self.date_time.time(),
+        # }
+        get_year = (lambda date: date.year)(self.date_time)
+        get_month = (lambda date: date.month)(self.date_time)
+        get_day = (lambda date: date.day)(self.date_time)
+        get_time = (lambda date: date.time())(self.date_time)
+
+        return {
+            'year': get_year,
+            'month': get_month,
+            'day': get_day,
+            'time': get_time
+        }
+
+    def is_number(self):
+        return list(map(lambda number: str(number).isdigit(), self.string_number_list))
+
+    def create_fibonacci(self, n):
+        fibonacci_series = reduce(lambda x, _: x.append(sum(x[-2:])) or x, range(2, n), [0, 1])
+        return fibonacci_series
+
+    def find_intersection(self):
+        return list(filter(lambda number: number in self.numbers1, self.numbers2))
+
+
 lambda_solutions = LambdaSolutions()
+print(lambda_solutions.find_intersection())
+# print(lambda_solutions.create_fibonacci(2))
+# print(lambda_solutions.is_number())
+# print(lambda_solutions.extract_date_time())
 # print(lambda_solutions.add_numbers(10))
 # print(lambda_solutions.multiply_number(5, 15))
 # print(lambda_solutions.operations_numbers(15))
 # print(lambda_solutions.sort_given_list())
 # print(lambda_solutions.sort_given_dict())
-print(lambda_solutions.filter_even_odd())
+# print(lambda_solutions.filter_even_odd())
+# print(lambda_solutions.check_length())
+# print(lambda_solutions.make_square_list())
+# print(lambda_solutions.make_cube_list())
+# print(lambda_solutions.check_starting_work())

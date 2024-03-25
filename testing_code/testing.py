1bafc3f57b26b5afb8f47333eada653ea3d0be95
ZygimantasB <92627436+ZygimantasB@users.noreply.github.com>
2024-03-25 20:07:47 +0200

    testing github push

--- testing_code/testing.py
+++ testing_code/testing.py
@@ -8,5 +8,6 @@ def add_lists(number_list1, number_list2):
     return list(map(list.__add__, number_list1, number_list2))
 
 
+
 # print(add_lists([[1, 3], [5, 7], [9, 11]], [[2, 4], [6, 8], [10, 12, 14]]))
 print(contains_list([1, 2, 3], [1, 2, 3, 4, 5]))
